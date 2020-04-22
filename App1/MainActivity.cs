a9a39390b0cfba1863fa2f26e46ab8947bd5abf5
jakubk97 <jkuba-kar@o2.pl>
2020-04-22 21:08:42 +0200

    project
    project

---
+++ App1/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView text;
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            text = FindViewById<TextView>(Resource.Id.textBox);
+
+            FindViewById<Button>(Resource.Id.buttonKlik).Click += (o, e) =>
+            text.Text = (++number).ToString();
+        }
+
+
+	}
+}
+
